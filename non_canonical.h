47c0bae0f5ff72ea0b183ce7896a9be1ab598799
Dhawal Jain <dhawalvrr@gmail.com>
2018-08-26 13:08:06 +0530

    MODE SHIFT FROM NON CANNO TO CANNO

--- non_canonical.h
+++ non_canonical.h
@@ -8,26 +8,26 @@
 #include <sys/types.h> 
 #include<dirent.h>
 #include<bits/stdc++.h>
-#include "minimal.h"
+//#include "minimal.h"
 //#include"canonical.h"
-#include "ls.h"
-#include "copy.h"
+//#include "ls.h"
+//#include "copy.h"
 //#include "xdg_open.h"
 #define clear() printf("\033[H\033[J")
 #define gotoxy(x,y) printf("\033[%d;%dH", (x), (y))
 using namespace std;
 void non_canonical()
 {
-printf("Welcome to Command Mode\n")
+	printf("                                              Welcome to Command Mode                                               \n");
+gotoxy(29,0);
+printf("...........................................................................................................................");
+gotoxy(30,0);
+
 printf("Enter Your Command\n");
 vector<string> v;
-string x;
-while(cin>>x)
-{
-v.push_back(x);
-}
-int n=v.size();
-if(!strcmp(v[0],"copy"));
-copy(v);
+int n,i,k;
+scanf("%d",&n);
+printf("%d",n);
+scanf("%d",&k);
 }
 
