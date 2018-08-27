26197e25b57b4e1ddbdcfc87e5c93aff1e6865b8
Dhawal Jain <dhawalvrr@gmail.com>
2018-08-27 10:33:20 +0530

    create dir,create file on home or some other is done

--- non_canonical.h
+++ non_canonical.h
@@ -19,6 +19,9 @@
 using namespace std;
 void non_canonical()
 {
+	//while(1)
+	//{
+		//clear();
 	printf("                                              Welcome to Command Mode                                               \n");
 gotoxy(29,0);
 printf("...........................................................................................................................");
@@ -43,8 +46,65 @@ if(b==0)
 break;
 }
 }
-printf("Copied");
+}
+///1 create directory
+///2 create file
+///3 create file inside directory
+if(strcmp(v[0].c_str(),"create_dir")==0)
+{
+for(i=1;i<n;i++)
+{
+	if(n>2)
+	{
+		string s=v[n-1]+"/"+v[i];
+        const char *folderadd= s.c_str();
+struct stat st = {0};
+ if (stat(folderadd, &st) == -1)
+ mkdir(folderadd,666);
+ else
+ printf("Folder Exist");
+   }
+   else
+   {
+   	 const char *folderadd= v[i].c_str();
+struct stat st = {0};
+ if (stat(folderadd, &st) == -1)
+ mkdir(folderadd,666);
+ else
+ printf("Folder Exist");
+   }
+}
+}
+if(strcmp(v[0].c_str(),"create_file")==0)
+{
+	const char* folderr;
+    //folderr = "C:\\Users\\SaMaN\\Desktop\\Ppln";
+    folderr = v[n-1].c_str();
+    struct stat sb;
+
+    if (stat(folderr, &sb) == 0 && S_ISDIR(sb.st_mode))
+    {
+    	for(i=1;i<n-1;i++)
+    	{
+         string s=v[n-1]+"/"+v[i];
+         char *fileadd=(char *)s.c_str();
+         FILE *fp;
+        fp=fopen(fileadd,"w");
+        fclose(fp);
+        }
+    }
+    else
+    {
+    	for(i=0;i<n;i++)
+    	{
+    	FILE *fp;
+        fp=fopen(v[i].c_str(),"w");
+        fclose(fp);
+        }
+    }
+}
 scanf("%d",&k);
+
 }
-}
+//}
 
