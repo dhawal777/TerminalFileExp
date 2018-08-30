24600feb4fbde186f6917c04add22eb854296bda
Dhawal Jain <dhawalvrr@gmail.com>
2018-08-30 11:15:55 +0530

    Main file terminal2 updated

--- terminal2.cpp
+++ terminal2.cpp
@@ -8,12 +8,9 @@
 #include <sys/types.h> 
 #include<dirent.h>
 #include<bits/stdc++.h>
-#include "minimal.h"
-//#include"canonical.h"
-#include "ls.h"
+#include<sys/ioctl.h>
+#include "lsls.h"
 #include "non_canonical.h"
-//#include "copy.h"
-//#include "xdg_open.h"
 #define clear() printf("\033[H\033[J")
 #define gotoxy(x,y) printf("\033[%d;%dH", (x), (y))
 using namespace std;
@@ -21,7 +18,12 @@ stack<string> stack1,stack2;
 
 void func(string s)
 {
-clear();
+ clear();
+ struct winsize w;
+ ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    //printf ("lines %d\n", w.ws_row);
+    //printf ("columns %d\n", w.ws_col);
+ int r=0;
 printf("%s\n",s.c_str());
 struct dirent **namelist;
     	int n;
@@ -29,160 +31,220 @@ struct dirent **namelist;
     		n=scandir(s.c_str(),&namelist,NULL,alphasort);
     			
     		int i;
-                for(i=0;i<n;i++)
-    		{
-    			printf("%s\n",namelist[i]->d_name);
-  
-  		}
+lsls(s,0);
  int x,y; 
 gotoxy(1,1);
 x=1;
 y=1;
 while(1){
 
-char j;
+char j,j1,j2;
 j=getchar();
-printf("%c",j);
-if(j=='A')
-{
-x--;
-gotoxy(x,y);
-}
-if(j=='B')
-{
-x++;
-gotoxy(x,y);
-}
 if(j=='\n')
 {
 //string s;
-s=s+"/"+namelist[x-2]->d_name;
 stack1.push(s);
-printf("%s\n",s);
-func(s);
-
-}
-if(j=='D')
-{
-printf("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\n");
-string s1=stack1.top();
-stack1.pop();
-stack2.push(s1);
-func(s1);
-
-
-}
-if(j=='C')
-{
-string s1=stack2.top();
-stack2.pop();
-stack1.push(s1);
-func(s1);
-
-}
-
-
-}
-}
-int main(int argc,char *argv[])
-{
-
-clear();
-
-
-
-    printf("You Started In Normal Mode");
-    printf("Press CTRL+C or Q to quit.\n");
- struct dirent **namelist;
-    	int n;
-      
-    		n=scandir(argv[1],&namelist,NULL,alphasort);
-    int p=n;
-int i;
-    		for(i=0;i<n;i++)
-    		{
-                      
-    			printf("%s\n",namelist[i]->d_name);
-    			
-    		}
-    		
-
-gotoxy(1,1);
-struct termios initial_settings, new_settings;
-tcgetattr(0,&initial_settings);
-new_settings = initial_settings;
-new_settings.c_lflag &= ~ICANON;
-new_settings.c_lflag &= ~ECHO;
-if(tcsetattr(0, TCSANOW, &new_settings) != 0) 
-fprintf(stderr,"could not set attributes\n");
-printf("dhawal");
-
-printf("p=%d\n",p);
-int x,y;
-x=1;
-y=1;
-
-
-
-while(1){
-
-char j;
-j=getchar();
-printf("%c",j);
-if(j=='A')
-{
-x--;
-gotoxy(x,y);
-}
-if(j=='B')
-{
-x++;
-gotoxy(x,y);
-}
-if(j=='\n')
-{
-string s;
-s=argv[1];
 s=s+"/"+namelist[x-2]->d_name;
-/***************************
-**************************/
+//stack1.push(s);
+printf("%s\n",s);
 if(namelist[x-2]->d_type==8)
 {
 string p ="xdg-open "+s;
 system(p.c_str());
 }
-//xdg_open(s.c_str());
 else
 {
-stack1.push(s);
+//stack1.push(s);
 printf("%s\n",s);
 func(s);
 }
 }
-/*******************
-***********************/
-if(j=='D')
+else if(j=='h')
 {
-printf("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\n");
+string p=".";
+stack2.push(s);
+func(p);
+}
+else if(j==127)
+{
+//printf("%cCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC",j2);
 string s1=stack1.top();
 stack1.pop();
-stack2.push(s1);
+stack2.push(s);
+func(s1);
+}
+/*else if(j==':')
+{
+    tcsetattr(0,TCSANOW,&initial_settings);
+    //clear();
+    non_canonical();
+
+}*/
+else
+{
+j1=getchar();
+j2=getchar();
+printf("jjjjjjjjjjjjjjjjjjjjjjj= %c",j2);
+if(j2=='A')
+{
+x--;
+if(x<=r)
+lsls(s,r--);
+gotoxy(x,y);
+}
+if(j2=='B')
+{
+++x;
+if(x>w.ws_row)
+    lsls(s,r++);
+gotoxy(x,y);
+}
+
+if(j2=='D')
+{
+string s1=stack1.top();
+stack1.pop();
+stack2.push(s);
 func(s1);
 
+
 }
-if(j=='C')
+if(j2=='C')
 {
 string s1=stack2.top();
 stack2.pop();
-stack1.push(s1);
+stack1.push(s);
 func(s1);
 
 }
-if(j==':')
-{tcsetattr(0,TCSANOW,&initial_settings);
-//non_canonical();
+}
+}
+}
+int main(int argc,char *argv[])
+{
+  //  struct winsize w;
+   // ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    //printf ("lines %d\n", w.ws_row);
+    //printf ("columns %d\n", w.ws_col);
+//printf("hello\n");
+
 clear();
-non_canonical();
+
+
+    struct winsize w;
+ ioctl(STDOUT_FILENO, TIOCGWINSZ, &w); 
+ 
+    printf("Welcome to Normal Mode\n");
+    //printf("Press CTRL+C or Q to quit.\n");
+    struct dirent **namelist;
+    int n;
+    int r=0;
+    n=scandir(argv[1],&namelist,NULL,alphasort);
+    int p=n;
+    int i;
+    lsls(argv[1],0);
+     r++;
+    gotoxy(1,1);
+    struct termios initial_settings, new_settings;
+    tcgetattr(0,&initial_settings);
+    new_settings = initial_settings;
+    new_settings.c_lflag &= ~ICANON;
+    new_settings.c_lflag &= ~ECHO;
+    if(tcsetattr(0, TCSANOW, &new_settings) != 0) 
+    fprintf(stderr,"could not set attributes\n");
+    printf("dhawal");
+
+    printf("p=%d\n",p);
+    printf ("lines %d\n", w.ws_row);
+    int x,y;
+    x=1;
+    y=1;
+
+while(1){
+string s;
+s=argv[1];
+char j,j1,j2;
+j=getchar();
+if(j=='\n')
+{
+stack1.push(s);
+s=s+"/"+namelist[x-2]->d_name;
+if(namelist[x-2]->d_type==8)
+{
+string p ="xdg-open "+s;
+system(p.c_str());
+}
+else
+{
+//stack1.push(s);
+printf("%s\n",s);
+func(s);
+}
+}
+else if(j=='h')
+{
+string p=".";
+stack2.push(p);
+func(p);
+}
+else if(j==127)
+{
+//printf("%cCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC",j2);
+string s1=stack1.top();
+stack1.pop();
+stack2.push(s);
+func(s1);
+}
+else if(j==':')
+{
+    tcsetattr(0,TCSANOW,&initial_settings);
+    //clear();
+    non_canonical();
+
+}
+else
+{
+j1=getchar();
+j2=getchar();
+//printf("%cllllllllllllllllllllllllllllllllllllllllllllllll",j2);
+    if(j2=='A'&&x>=1)
+    {
+    x--;
+    if(x<=r&&x>=0)
+        {clear();
+    lsls(s,r--);
+    }
+    gotoxy(x,y);
+    }
+    if(j2=='B')
+    {
+    x++;
+    if(x>w.ws_row-10-r)
+    {
+        clear();
+      lsls(s,r++);
+    }
+    gotoxy(x-r,y);
+    }
+
+    if(j2=='D')
+    {
+    //printf("%cCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC",j2);
+    string s1=stack1.top();
+    stack1.pop();
+    stack2.push(s);
+    func(s1);
+
+    }
+    if(j2=='C')
+    {
+    //printf("%cllllllllllllllllllllllllllllllllllllllllllllllll",j2);
+    string s1=stack2.top();
+    stack2.pop();
+    stack1.push(s);
+    func(s1);
+
+}
 }
 }
 tcsetattr(0,TCSANOW,&initial_settings);
