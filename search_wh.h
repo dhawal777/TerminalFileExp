82022fb97dc0ecdcddbef1785f4ecb497c971c12
Dhawal Jain <dhawalvrr@gmail.com>
2018-09-02 16:07:36 +0530

    search whole with back arrow

--- search_wh.h
+++ search_wh.h
@@ -1,6 +1,14 @@
 #include <pwd.h>
 #include <grp.h>
 #include<time.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>                   
+#include <string>
+#include <errno.h>
+#include <sys/stat.h> 
+#include <sys/types.h> 
 #include<dirent.h>
 #include<sys/ioctl.h>
 #include<bits/stdc++.h>
@@ -13,8 +21,9 @@
 #define gotoxy(x,y) printf("\033[%d;%dH", (x), (y))
 using namespace std;
 bool ans1=false;
-vector<string> v;
-void search2(char *dir, int depth,char *check)
+vector<string> v,v1;
+stack<string> stack3,stack4;
+void search2(char *dir, int depth,char *check,string s)
 {
 DIR *dp;
 
@@ -41,7 +50,7 @@ ans1=true;
 
 //printf("%*s%s/\n",depth,"",entry->d_name);
 /* Recurse at a new indent level */
-search2(entry->d_name,depth+4,check);
+search2(entry->d_name,depth+4,check,s);
 }
 else 
 {
@@ -51,6 +60,7 @@ if(strcmp(entry->d_name,check)==0)
 	string s=dir;
 	s=s+s1+entry->d_name;
 	v.push_back(s);
+	v1.push_back(entry->d_name);
 	//cout<<"hello strcmp"<<endl;
     ans1=true;
 }
@@ -62,12 +72,18 @@ closedir(dp);
 clear();
 gotoxy(1,1);
 for(int i=0;i<v.size();i++)
-   cout<<v[i]<<endl;
-//gotoxy(29,0);
+   {printf("%s\n",(char*)v[i].c_str());
+   	gotoxy(i+1,30);
+   	printf("%s\n",(char*)v1[i].c_str());
+   }
+gotoxy(29,0);
 struct winsize w;
 ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 gotoxy(w.ws_row-8,0);
 printf("                                              Welcome to Command Mode                                               \n");
 printf("...........................................................................................................................");
 gotoxy(w.ws_row-6,0);
+gotoxy(1,1);
+int x=1;
+ 	int y=1;
 }
